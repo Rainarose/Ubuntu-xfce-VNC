d37e1d36c678ba36eca00852a128c07a386d832c
Dayo <1690328478@qq.com>
2020-03-01 07:18:06 +0800

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -4,7 +4,7 @@ apt-get -y install xfce4
 apt-get -y install vnc4server
 vncserver :2
 vncserver -kill :2
-cd /root
+cd /home/admin/
 wget https://raw.githubusercontent.com/chengziqaq/Ubuntu-xfce-VNC/master/xstartup.txt
 mv xstartup.txt /home/admin/.vnc/xstartup
 cd /home/admin/.vnc/
