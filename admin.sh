4c40e3400051078021e50a0c0eee7d4fa941674a
Dayo <1690328478@qq.com>
2020-03-01 07:17:21 +0800

    Create admin.sh

---
+++ admin.sh
@@ -0,0 +1,15 @@
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install xfce4
+apt-get -y install vnc4server
+vncserver :2
+vncserver -kill :2
+cd /root
+wget https://raw.githubusercontent.com/chengziqaq/Ubuntu-xfce-VNC/master/xstartup.txt
+mv xstartup.txt /home/admin/.vnc/xstartup
+cd /home/admin/.vnc/
+chmod 755 xstartup
+apt-get install -y firefox
+sudo apt-get install xfonts-wqy
+echo "重启中,请稍等"
+reboot
