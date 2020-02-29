870a062d6014031f98dfc991ed4be09bc827fdfc
Dayo <1690328478@qq.com>
2020-03-01 02:14:02 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+sudo su root
+apt-get update
+apt-get install xfce4
+apt-get -y install vnc4server
+vncserver :1
+vncserver -kill :1
+cd /root
+wget https://raw.githubusercontent.com/chengziqaq/Ubuntu-xfce-VNC/master/xstartup.txt
+mv xstartup.txt /root/.vnc/xstartup
+cd /root/.vnc/
+chmod 755 xstartup
+reboot
