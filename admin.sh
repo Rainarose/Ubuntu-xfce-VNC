f16ee719ac4b18c459e7420755f3f5c7ac7791d7
Dayo <1690328478@qq.com>
2020-03-01 07:53:11 +0800

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -1,7 +1,11 @@
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install xfce4
-apt-get -y install vnc4server
+echo "安装chrome"  
+sudo wget http://www.linuxidc.com/files/repo/google-chrome.list -P /etc/apt/sources.list.d/  
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -    
+sudo apt-get -y update  
+sudo apt-get install google-chrome-stable  
+sudo apt-get -y upgrade
+sudo apt-get -y install xfce4
+sudo apt-get -y install vnc4server
 vncserver :2
 vncserver -kill :2
 cd /home/admin/
@@ -9,7 +13,9 @@ wget https://raw.githubusercontent.com/chengziqaq/Ubuntu-xfce-VNC/master/xstartu
 mv xstartup.txt /home/admin/.vnc/xstartup
 cd /home/admin/.vnc/
 chmod 755 xstartup
-apt-get install -y firefox
+sudo apt-get install -y firefox
 sudo apt-get -y install ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy
+
+
 echo "重启中,请稍等"
 reboot
