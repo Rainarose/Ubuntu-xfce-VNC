99ccbf7e3b3d4b452ed1f947f9b8dafc223d2518
Dayo <1690328478@qq.com>
2020-03-01 07:55:03 +0800

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -2,7 +2,7 @@ echo "安装chrome"
 sudo wget http://www.linuxidc.com/files/repo/google-chrome.list -P /etc/apt/sources.list.d/   
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -    
 sudo apt-get -y update  
-sudo apt-get install google-chrome-stable   
+sudo apt-get -y install google-chrome-stable   
 sudo apt-get -y upgrade  
 sudo apt-get -y install xfce4  
 sudo apt-get -y install vnc4server  
